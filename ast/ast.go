96a37683f255670166e2881f6077a38c952d4b0a
arifaydin16 - Laptop <flaris55@hotmail.com>
2026-05-11 17:00:29 +0300

    ast [1]

--- ast/ast.go
+++ ast/ast.go
@@ -6,42 +6,91 @@ import (
 	"strconv"
 )
 
+func ParseAssignment(tokens []utils.Token, token utils.Token, i int) utils.AssignmentStatement {
+	var assignmentValue interface{} = tokens[i+2].Literal
+	assignmentType := tokens[i-1].Literal
+	var err error
+
+	// Tip dönüşümlerini güvenli hale getirelim
+	switch assignmentType {
+	case "int":
+		// Literal her zaman string gelir, direkt parse edebiliriz
+		assignmentValue, err = strconv.ParseInt(tokens[i+2].Literal, 10, 64)
+	case "float":
+		assignmentValue, err = strconv.ParseFloat(tokens[i+2].Literal, 64)
+	case "bool":
+		assignmentValue, err = strconv.ParseBool(tokens[i+2].Literal)
+	}
+
+	if err != nil {
+		fmt.Printf("Conversation error: in %s line %s excepted.\n", string(i+2), assignmentType)
+	}
+
+	var ass = utils.AssignmentStatement{
+		Variable: token.Literal,
+		Scope:    tokens[i-2].Literal,
+		Type:     tokens[i-1].Literal,
+		Value:    assignmentValue,
+	}
+	return ass
+}
+
+func ParseComparison(tokens []utils.Token, i int) utils.ComparisonExpression {
+	var token = tokens[i]
+	var com = utils.ComparisonExpression{}
+	if token.Literal == "IDENTIFIER" && tokens[i+1].Literal == "COMPARISON" {
+		com.Variable = token.Literal
+		com.Comparison = tokens[i+1].Type
+		com.Compared = tokens[i+2].Literal
+	}
+	return com
+}
+func ParseUnary(tokens []utils.Token, i int) utils.UnaryStatement {
+	var token = tokens[i]
+	var un = utils.UnaryStatement{}
+	if token.Literal == "UNARY" && tokens[i-1].Literal == "IDENTIFIER" {
+		un.Variable = tokens[i-1].Literal
+		un.Unary = token.Literal
+	}
+	return un
+}
+
 func ParseARLang(tokens []utils.Token) *utils.Codebase {
 	var program = &utils.Codebase{}
 	for i := 0; i < len(tokens); i++ {
 		token := tokens[i]
 		if token.Type == "IDENTIFIER" {
 			if i+1 < len(tokens) && tokens[i+1].Literal == "=" {
-				var assignmentValue interface{} = tokens[i+2].Literal
-				assignmentType := tokens[i-1].Literal
-				var err error
-
-				// Tip dönüşümlerini güvenli hale getirelim
-				switch assignmentType {
-				case "int":
-					// Literal her zaman string gelir, direkt parse edebiliriz
-					assignmentValue, err = strconv.ParseInt(tokens[i+2].Literal, 10, 64)
-				case "float":
-					assignmentValue, err = strconv.ParseFloat(tokens[i+2].Literal, 64)
-				case "bool":
-					assignmentValue, err = strconv.ParseBool(tokens[i+2].Literal)
-				}
-
-				if err != nil {
-					fmt.Printf("Conversation error: in %v line %s excepted.\n", tokens[i+2].Line, assignmentType)
-				}
-
-				assignment := utils.AssignmentStatement{
-					Variable: token.Literal,
-					Scope:    tokens[i-2].Literal,
-					Type:     tokens[i-1].Literal,
-					Value:    assignmentValue,
-				}
-
+				var assignment = ParseAssignment(tokens, token, i)
 				program.Statements = append(program.Statements, assignment)
 
-				// Atama satırını tükettik
 				i += 2
+			}
+		} else if token.Type == "KEYWORD" {
+			switch token.Literal {
+			case "for":
+				var ForStatement = &utils.ForStatement{}
+				i++
+				if token.Type == "PAREN" && token.Literal == "{" {
+					i++
+				}
+				for tokens[i].Literal != ";" {
+					i++
+				}
+
+				var assStatement = ParseAssignment(tokens, token, i)
+				ForStatement.Init = assStatement
+				for tokens[i].Literal != ";" {
+					i++
+				}
+				var comStatement = ParseComparison(tokens, i)
+				ForStatement.Condition = comStatement
+				for tokens[i].Literal != ";" {
+					i++
+				}
+				var unaryStatement = ParseUnary(tokens, i+1)
+				ForStatement.Post = unaryStatement
+
 			}
 		}
 	}
--- utils/types.go
+++ utils/types.go
@@ -1,5 +1,10 @@
 package utils
 
+type Parser struct {
+	tokens []Token
+	pos    int
+}
+
 type Token struct {
 	Type    string
 	Literal string
@@ -25,13 +30,45 @@ type Statement interface {
 	statementNode()
 }
 
+type BlockStatement struct {
+	Statements []Statement
+}
+
+type ComparisonExpression struct {
+	Variable   string
+	Comparison string
+	Compared   string
+}
+
+type UnaryStatement struct {
+	Variable string
+	Unary    string
+}
+
+func (ce ComparisonExpression) expressionNode()      {}
+func (ce ComparisonExpression) TokenLiteral() string { return ce.Variable }
+
 type AssignmentStatement struct {
 	Variable string
 	Scope    string
 	Type     string
-	Value    string
+	Value    interface{}
 }
 
+type ForStatement struct {
+	Init      AssignmentStatement
+	Condition Expression
+	Post      Statement
+	Body      *[]BlockStatement
+}
+
+func (bs BlockStatement) statementNode()            {}
+func (bs ForStatement) statementNode()              {}
+func (bs AssignmentStatement) statementNode()       {}
+func (bs AssignmentStatement) TokenLiteral() string { return bs.Variable }
+func (bs UnaryStatement) statementNode()            {}
+func (bs UnaryStatement) TokenLiteral() string      { return bs.Variable }
+
 type Codebase struct {
-	Statements []AssignmentStatement
+	Statements []Statement
 }
